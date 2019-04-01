d675c834e5795507ecb22a8bc50e62d809fb13ef
Henderson Hood <hendy_home_pycharm@hendy.local>
2019-03-31 21:00:38 -0400

    add tests for the add and product functions.

--- test_math_func.py
+++ test_math_func.py
@@ -1,5 +1,10 @@
 import math_func
+
 def test_add():
-    assert math_func.add(1,3) == 10
-    assert math_func.product(5,5) == 25
+    assert math_func.add(1, 3) == 4
+    assert math_func.product(5, 5) == 25
+    assert math_func.product(5) == 10
+
+def test_product():
+    assert math_func.product(5, 5) == 25
     assert math_func.product(5) == 10
